c809b0717102f56b8de6ed001a57663af072278a
DarkComLikeEating <87956422+DarkComLikeEating@users.noreply.github.com>
2023-03-25 14:29:32 +0800

    链队和链栈更新

---
+++ 03 栈和队列/链栈.cpp
@@ -0,0 +1,65 @@
+/*
+栈的链式存储（链栈）
+初始化栈 InitStack(&s)
+销毁栈 DestroyStack(&s)
+判断栈是否为空 StackEmpty(s)
+进栈 Push(&s, e)
+出栈 Pop(&s, &e)
+取栈顶元素 GetTop(s, &e) 
+*/ 
+
+#include <iostream>
+#include <malloc.h>
+
+using namespace std;
+
+typedef struct linknode {
+	int data;
+	struct linknode * next;
+} LinkStNode;
+
+void InitStack(LinkStNode * &s) {
+	s = (LinkStNode * ) malloc (sizeof(LinkStNode));
+	s->next = NULL;
+}
+
+void DestroyStack(LinkStNode * &s) {
+	LinkStNode * pre = s, * p = s->next;
+	while (p != NULL) {
+		free(pre);
+		pre = p;
+		p = pre->next;
+	}
+	free(pre);
+}
+
+bool StackEmpty(LinkStNode * s) {
+	return (s->next == NULL);
+}
+
+void Push(LinkStNode * &s, int e) {
+	LinkStNode * p;
+	p = (LinkStNode * ) malloc (sizeof(LinkStNode));
+	p->next = s->next;
+	s->next = p;
+}
+
+bool Pop(LinkStNode * &s, int &e) {
+	LinkStNode * p;
+	if (s->next == NULL) {
+		return false;
+	}
+	p = s->next;
+	e = p->data;
+	s->next = p->next;
+	free(p);
+	return true;
+}
+
+bool GetTop(LinkStNode * &s, int &e) {
+	if (s->next == NULL) {
+		return false;
+	}
+	e = s->next->data;
+	return true;
+}
---
+++ 03 栈和队列/链队.cpp
@@ -0,0 +1,11 @@
+/*
+队列的链式存储 
+初始化队列 InitQueue(&q) 
+销毁队列 DestroyQueue(&q)
+判断队列是否为空 QueueEmpty(q)
+进队列 EnQueue(&q, e)
+出队列 DeQueue(&q, &e)
+*/
+
+#include <iostream>
+
